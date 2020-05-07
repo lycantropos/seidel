55530d7ef66eaac2ff64bb8900e6402236140fcf
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-07 11:53:46 +0300

    Add bound 'Point' class

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,17 @@ namespace py = pybind11;
 #define MODULE_NAME _seidel
 #define C_STR_HELPER(a) #a
 #define C_STR(a) C_STR_HELPER(a)
+#define POINT_NAME "Point"
 
 PYBIND11_MODULE(MODULE_NAME, m) {
   m.doc() = R"pbdoc(
         Python binding of randomized algorithm for trapezoidal decomposition by R. Seidel.
     )pbdoc";
+
+  py::class_<Point>(m, POINT_NAME)
+      .def(py::init<double, double>(), py::arg("x") = 0., py::arg("y") = 0.)
+      .def_readwrite("x", &Point::x)
+      .def_readwrite("y", &Point::y);
 
 #ifdef VERSION_INFO
   m.attr("__version__") = VERSION_INFO;
