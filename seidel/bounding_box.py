5dc8c371b07959c0d0aff8bba33a9defd463c551
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-07 20:17:56 +0300

    Add ported 'BoundingBox.__eq__' magic method

--- seidel/bounding_box.py
+++ seidel/bounding_box.py
@@ -8,3 +8,10 @@ class BoundingBox:
         self.empty = empty
         self.lower = lower
         self.upper = upper
+
+    def __eq__(self, other: 'BoundingBox') -> bool:
+        return ((self.empty is other.empty
+                 and self.lower == other.lower
+                 and self.upper == other.upper)
+                if isinstance(other, BoundingBox)
+                else NotImplemented)
