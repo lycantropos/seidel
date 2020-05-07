a6f0bd8a45779796fec126ba77ede830decd5895
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-07 20:40:15 +0300

    Add test for bound & ported 'BoundingBox' classes pickling

---
+++ tests/integration_tests/bounding_boxes_tests/test_pickle.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+from _seidel import BoundingBox as Bound
+from hypothesis import given
+
+from seidel.bounding_box import BoundingBox as Ported
+from tests.utils import (are_bound_ported_bounding_boxes_equal,
+                         pickle_round_trip)
+from . import strategies
+
+
+@given(strategies.bounding_boxes_pairs)
+def test_round_trip(bounding_boxes_pair: Tuple[Bound, Ported]) -> None:
+    bound, ported = bounding_boxes_pair
+
+    assert are_bound_ported_bounding_boxes_equal(pickle_round_trip(bound),
+                                                 pickle_round_trip(ported))
