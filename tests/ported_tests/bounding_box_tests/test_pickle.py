c5253470a563fac2c357038f018f090bf352aa4d
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-07 20:20:17 +0300

    Add test for ported 'BoundingBox' class pickling

---
+++ tests/ported_tests/bounding_box_tests/test_pickle.py
@@ -0,0 +1,10 @@
+from hypothesis import given
+
+from seidel.bounding_box import BoundingBox
+from tests.utils import pickle_round_trip
+from . import strategies
+
+
+@given(strategies.bounding_boxes)
+def test_round_trip(bounding_box: BoundingBox) -> None:
+    assert pickle_round_trip(bounding_box) == bounding_box
