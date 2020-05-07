5023ce77a4388ff05f3432d9263589cde4b4d30a
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-07 19:53:31 +0300

    Add test for bound 'BoundingBox' class pickling

---
+++ tests/bind_tests/bounding_box_tests/test_pickle.py
@@ -0,0 +1,10 @@
+from _seidel import BoundingBox
+from hypothesis import given
+
+from tests.utils import pickle_round_trip
+from . import strategies
+
+
+@given(strategies.bounding_boxes)
+def test_round_trip(bounding_box: BoundingBox) -> None:
+    assert pickle_round_trip(bounding_box) == bounding_box
