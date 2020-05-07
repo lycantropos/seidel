ec85d98d6bb7b84bd9126fe2ac1ba9d4d1fb5199
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-07 18:02:28 +0300

    Add ported 'Point.__eq__' magic method

--- seidel/point.py
+++ seidel/point.py
@@ -7,3 +7,8 @@ class Point:
     def __init__(self, x: Coordinate, y: Coordinate) -> None:
         self.x = x
         self.y = y
+
+    def __eq__(self, other: 'Point') -> None:
+        return (self.x == other.x and self.y == other.y
+                if isinstance(other, Point)
+                else NotImplemented)
