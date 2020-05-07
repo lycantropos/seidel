2b5e9f3fc5d0ada4a909b8023ff165b125203200
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-07 18:52:25 +0300

    Add test for bound & ported 'Point' classes pickling

---
+++ tests/integration_tests/points_tests/test_pickle.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+from _seidel import Point as Bound
+from hypothesis import given
+
+from seidel.point import Point as Ported
+from tests.utils import (are_bound_ported_points_equal,
+                         pickle_round_trip)
+from . import strategies
+
+
+@given(strategies.points_pairs)
+def test_round_trip(points_pair: Tuple[Bound, Ported]) -> None:
+    bound, ported = points_pair
+
+    assert are_bound_ported_points_equal(pickle_round_trip(bound),
+                                         pickle_round_trip(ported))
