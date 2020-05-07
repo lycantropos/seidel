43c478ecd7e8c5556a623cb76d3f151c0ec93637
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-07 14:34:49 +0300

    Add test for bound 'Point.__eq__' magic method

---
+++ tests/bind_tests/point_tests/test_equals.py
@@ -0,0 +1,33 @@
+from _seidel import Point
+from hypothesis import given
+
+from tests.utils import (equivalence,
+                         implication)
+from . import strategies
+
+
+@given(strategies.points)
+def test_reflexivity(point: Point) -> None:
+    assert point == point
+
+
+@given(strategies.points, strategies.points)
+def test_symmetry(first_point: Point,
+                  second_point: Point) -> None:
+    assert equivalence(first_point == second_point,
+                       second_point == first_point)
+
+
+@given(strategies.points, strategies.points, strategies.points)
+def test_transitivity(first_point: Point, second_point: Point,
+                      third_point: Point) -> None:
+    assert implication(first_point == second_point
+                       and second_point == third_point,
+                       first_point == third_point)
+
+
+@given(strategies.points, strategies.points)
+def test_connection_with_inequality(first_point: Point,
+                                    second_point: Point) -> None:
+    assert equivalence(not first_point == second_point,
+                       first_point != second_point)
--- tests/utils.py
+++ tests/utils.py
@@ -7,3 +7,11 @@ Strategy = SearchStrategy
 MAX_FLOAT_DIGITS_COUNT = sys.float_info.dig // 2
 MAX_VALUE = 10 ** 6
 MIN_VALUE = -MAX_VALUE
+
+
+def equivalence(left_statement: bool, right_statement: bool) -> bool:
+    return left_statement is right_statement
+
+
+def implication(antecedent: bool, consequent: bool) -> bool:
+    return not antecedent or consequent
