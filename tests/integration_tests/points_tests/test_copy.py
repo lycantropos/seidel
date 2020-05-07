1ff87c95cb47e8ed6b39ba7e888a79936031434a
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-07 18:53:02 +0300

    Add test for bound & ported 'Point' classes copying

---
+++ tests/integration_tests/points_tests/test_copy.py
@@ -0,0 +1,24 @@
+import copy
+from typing import Tuple
+
+from _seidel import Point as Bound
+from hypothesis import given
+
+from seidel.point import Point as Ported
+from tests.utils import are_bound_ported_points_equal
+from . import strategies
+
+
+@given(strategies.points_pairs)
+def test_shallow(points_pair: Tuple[Bound, Ported]) -> None:
+    bound, ported = points_pair
+
+    assert are_bound_ported_points_equal(copy.copy(bound), copy.copy(ported))
+
+
+@given(strategies.points_pairs)
+def test_deep(points_pair: Tuple[Bound, Ported]) -> None:
+    bound, ported = points_pair
+
+    assert are_bound_ported_points_equal(copy.deepcopy(bound),
+                                         copy.deepcopy(ported))
