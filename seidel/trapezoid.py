5a2bd3dd04938857f435a7eb57ee1831a3ca146a
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-08 21:40:22 +0300

    Add ported 'Edge.__eq__' magic method

--- seidel/trapezoid.py
+++ seidel/trapezoid.py
@@ -14,3 +14,9 @@ class Trapezoid:
         self.right = right
         self.above = above
         self.below = below
+
+    def __eq__(self, other: 'Trapezoid') -> bool:
+        return ((self.left == other.left and self.right == other.right
+                 and self.above == other.above and self.below == other.below)
+                if isinstance(other, Trapezoid)
+                else NotImplemented)
