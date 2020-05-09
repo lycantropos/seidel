5e9a8c1e217a100bcf08b3f1dd484e43eadaea71
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-09 20:55:22 +0300

    Add ported 'Leaf.__repr__' magic method

--- seidel/leaf.py
+++ seidel/leaf.py
@@ -1,3 +1,5 @@
+from reprit.base import generate_repr
+
 from .node import Node
 from .trapezoid import Trapezoid
 
@@ -7,6 +9,8 @@ class Leaf(Node):
 
     def __init__(self, trapezoid: Trapezoid) -> None:
         self.trapezoid = trapezoid
+
+    __repr__ = generate_repr(__init__)
 
     def __eq__(self, other: 'Leaf') -> bool:
         return (self.trapezoid == other.trapezoid
