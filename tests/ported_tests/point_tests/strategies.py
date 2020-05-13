f3bbd9652b89e195bc92423655d35e0f5733377c
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-13 16:55:37 +0300

    Add test for ported 'Point.__sub__' magic method

--- tests/ported_tests/point_tests/strategies.py
+++ tests/ported_tests/point_tests/strategies.py
@@ -1,10 +1,48 @@
+from typing import Tuple
+
+from hypothesis import strategies
+
+from seidel.hints import Coordinate
+from seidel.point import Point
 from tests.strategies import (coordinates_strategies,
                               coordinates_to_ported_points,
                               to_pairs,
                               to_triplets)
+from tests.utils import Strategy
 
 coordinates_pairs = coordinates_strategies.flatmap(to_pairs)
 points = coordinates_strategies.flatmap(coordinates_to_ported_points)
+
+
+def coordinates_to_points_with_origins(coordinates: Strategy[Coordinate]
+                                       ) -> Strategy[Tuple[Point, Point]]:
+    return strategies.tuples(coordinates_to_ported_points(coordinates),
+                             coordinates_to_origins(coordinates))
+
+
+def coordinates_to_origins(coordinates: Strategy[Coordinate]
+                           ) -> Strategy[Point]:
+    def coordinate_to_origin(coordinate: Coordinate) -> Point:
+        domain = type(coordinate)
+        return Point(domain(), domain())
+
+    return coordinates.map(coordinate_to_origin)
+
+
+points_with_origins = (coordinates_strategies
+                       .flatmap(coordinates_to_points_with_origins))
+
+
+def coordinates_to_points_pairs_with_origins(coordinates: Strategy[Coordinate]
+                                             ) -> Strategy[Tuple[Point, Point,
+                                                                 Point]]:
+    points = coordinates_to_ported_points(coordinates)
+    return strategies.tuples(points, points,
+                             coordinates_to_origins(coordinates))
+
+
+points_pairs_with_origins = coordinates_strategies.flatmap(
+        coordinates_to_points_pairs_with_origins)
 points_strategies = coordinates_strategies.map(coordinates_to_ported_points)
 points_pairs = points_strategies.flatmap(to_pairs)
 points_triplets = points_strategies.flatmap(to_triplets)
---
+++ tests/ported_tests/point_tests/test_difference.py
@@ -0,0 +1,37 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from seidel.point import Point
+from . import strategies
+
+
+@given(strategies.points_pairs)
+def test_basic(points_pair: Tuple[Point, Point]) -> None:
+    left, right = points_pair
+
+    assert isinstance(left - right, Point)
+
+
+@given(strategies.points_with_origins)
+def test_right_neutral_element(point_with_origin: Tuple[Point, Point]) -> None:
+    point, origin = point_with_origin
+
+    assert point - origin == point
+
+
+@given(strategies.points_with_origins)
+def test_self_inverse(point_with_origin: Tuple[Point, Point]) -> None:
+    point, origin = point_with_origin
+
+    result = point - point
+
+    assert result == origin
+
+
+@given(strategies.points_pairs_with_origins)
+def test_equality_criteria(points_pair_with_origin: Tuple[Point, Point, Point]
+                           ) -> None:
+    left, right, origin = points_pair_with_origin
+
+    assert (left - right == origin) is (left == right)
