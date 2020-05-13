c18f9119e71584ba8c4240d56592d4a29887ed15
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-13 15:59:27 +0300

    Complete ported 'Trapezoid' class fields

--- seidel/trapezoid.py
+++ seidel/trapezoid.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from reprit.base import generate_repr
 
 from .edge import Edge
@@ -5,7 +7,9 @@ from .point import Point
 
 
 class Trapezoid:
-    __slots__ = 'left', 'right', 'above', 'below'
+    __slots__ = ('left', 'right', 'above', 'below',
+                 '_lower_left', '_lower_right', '_upper_left', '_upper_right',
+                 'trapezoid_node')
 
     def __init__(self,
                  left: Point,
@@ -17,6 +21,10 @@ class Trapezoid:
         self.right = right
         self.above = above
         self.below = below
+        self._lower_left = None  # type: Optional['Trapezoid']
+        self._lower_right = None  # type: Optional['Trapezoid']
+        self._upper_left = None  # type: Optional['Trapezoid']
+        self._upper_right = None  # type: Optional['Trapezoid']
 
     __repr__ = generate_repr(__init__)
 
@@ -25,3 +33,43 @@ class Trapezoid:
                  and self.above == other.above and self.below == other.below)
                 if isinstance(other, Trapezoid)
                 else NotImplemented)
+
+    @property
+    def lower_left(self) -> Optional['Trapezoid']:
+        return self._lower_left
+
+    @property
+    def lower_right(self) -> Optional['Trapezoid']:
+        return self._lower_right
+
+    @property
+    def upper_left(self) -> Optional['Trapezoid']:
+        return self._upper_left
+
+    @property
+    def upper_right(self) -> Optional['Trapezoid']:
+        return self._upper_right
+
+    @lower_left.setter
+    def lower_left(self, value: Optional['Trapezoid']) -> None:
+        self._lower_left = value
+        if value is not None:
+            value._lower_right = self
+
+    @lower_right.setter
+    def lower_right(self, value: Optional['Trapezoid']) -> None:
+        self._lower_right = value
+        if value is not None:
+            value._lower_left = self
+
+    @upper_left.setter
+    def upper_left(self, value: Optional['Trapezoid']) -> None:
+        self._upper_left = value
+        if value is not None:
+            value._upper_right = self
+
+    @upper_right.setter
+    def upper_right(self, value: Optional['Trapezoid']) -> None:
+        self._upper_right = value
+        if value is not None:
+            value._upper_left = self
