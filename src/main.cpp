d9d7c4aa634843559e7fcb6d6e43184c4782adf2
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-13 15:47:51 +0300

    Add bound 'Point.__mul__' magic method

--- src/main.cpp
+++ src/main.cpp
@@ -293,6 +293,7 @@ PYBIND11_MODULE(MODULE_NAME, m) {
       .def(py::self == py::self)
       .def(py::self + py::self)
       .def(py::self - py::self)
+      .def(py::self * float())
       .def("__repr__", repr<Point>)
       .def_readwrite("x", &Point::x)
       .def_readwrite("y", &Point::y)
