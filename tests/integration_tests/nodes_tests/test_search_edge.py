8b00d6e31e455408df7079e992a0dcee5711a4db
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-14 17:29:27 +0300

    Add test for bound & ported 'Node.search_edge' methods

---
+++ tests/integration_tests/nodes_tests/test_search_edge.py
@@ -0,0 +1,16 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedEdgesPair,
+                         BoundPortedNodesPair,
+                         are_bound_ported_trapezoids_equal)
+from . import strategies
+
+
+@given(strategies.nodes_pairs, strategies.edges_pairs)
+def test_round_trip(nodes_pair: BoundPortedNodesPair,
+                    edges_pair: BoundPortedEdgesPair) -> None:
+    bound, ported = nodes_pair
+    bound_edge, ported_edge = edges_pair
+
+    assert are_bound_ported_trapezoids_equal(bound.search_edge(bound_edge),
+                                             ported.search_edge(ported_edge))
