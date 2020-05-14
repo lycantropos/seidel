65bbad2d059eb457793fe753c2c83ba089fb5afd
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-14 16:11:58 +0300

    Complete strategies

--- tests/ported_tests/edge_tests/strategies.py
+++ tests/ported_tests/edge_tests/strategies.py
@@ -5,8 +5,6 @@ from tests.strategies import (coordinates_strategies,
                               to_pairs,
                               to_triplets)
 
-coordinates_pairs = coordinates_strategies.flatmap(to_pairs)
-points = coordinates_strategies.flatmap(coordinates_to_ported_points)
 sorted_points_pairs = (coordinates_strategies
                        .flatmap(coordinates_to_sorted_ported_points_pairs))
 edges = coordinates_strategies.flatmap(coordinates_to_ported_edges)
